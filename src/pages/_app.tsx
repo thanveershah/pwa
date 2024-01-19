51ed5f1cc1b5abd2492e08e5eb68d523bfd5bc6b
thanveer <thanveer@thanveers-MacBook-Pro.local>
2024-01-19 11:43:49 +0400

    updated code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const handleDeepLinking = () => {
+    const currentUrl = window.location.href;
+
+    if (currentUrl.includes("yourpwacustomscheme")) {
+      const route = currentUrl.replace("yourpwacustomscheme://", "");
+      window.location.href = `/${route}`;
+    }
+  };
+
+  useEffect(() => {
+    handleDeepLinking();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
